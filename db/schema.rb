dd0d8d1566b0adb0d7679d8167e08db1e2b27041
bunnyy <joewlozg@gmail.com>
2019-11-24 22:39:28 +0800

    createModel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_061050) do
+ActiveRecord::Schema.define(version: 2019_11_24_143403) do
+
+  create_table "money", force: :cascade do |t|
+    t.string "expense_type"
+    t.string "title"
+    t.integer "amount"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
