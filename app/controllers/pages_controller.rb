1e790dbcaae17d841bb408c54d2a1f8d4908cacb
bunnyy <joewlozg@gmail.com>
2019-11-25 23:01:52 +0800

    SetindexV2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def index
+    @money = Money.all
+  end
+
+  def create
+    @money = Money.new(find_params)
+    if @money.save
+      redirect_to root, notice: "新增成功！"
+    else
+      redirect_to root, notice: "新增失敗！"
+    end
+  end
+
+  private
+  def find_params
+    params.require(:money).permit(:expense_type, :title, :amount, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
+
+  namespace :api do
+    resources :money, only:[:index, :create, :update, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
